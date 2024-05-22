320d2971889a0a62de71acec369cdc4a5c5f98c8
Mico Febrian <sutanmico@gmail.com>
2024-05-22 14:17:24 +0700

    remove: gitignore

--- frontend/src/pages/Skills.tsx
+++ frontend/src/pages/Skills.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function Skills() {
   return (
     <div className="snap-start h-[800px] md:h-screen lg:h-[800px] max-h-screen flex items-center justify-center text-white">
